e7db1cbae56222adfba0a5a7588c138d6e433cd5
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-12-12 18:36:30 +0100

    Milestone Page makes a request and send data to MilestoneList

--- src/Pages/Milestones/Milestones.js
+++ src/Pages/Milestones/Milestones.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react';
-import MilestoneList from '../../Components/MilestoneList';
+import MilestoneList from '../../components/MilestoneList';
+import axios from 'axios';
 
 class Milestones extends Component {
-  render() {
-    return (
-      <div className="Milestones">
-        <MilestoneList/>
-      </div>
-    );
-  }
+	constructor(props){
+		super(props);
+		this.state={
+			data: [],
+		};
+	}
+
+	handleResponse(){
+		axios.get('http://localhost:3000/milestone/')
+		.then(res => {
+			console.log(res.data.milestones);
+			this.setState({
+        		data: res.data.milestones,
+      		});
+		})
+	}
+
+	render() {
+		this.handleResponse();
+		return (
+			<div className="Milestones">
+				<MilestoneList data={this.state.data}/>
+			</div>
+		);
+	}
 }
 
 export default Milestones;
