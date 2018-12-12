bd0994164f87be09ce1b73d869aea56540a31921
Luis de la Cal <luiscal4a@gmail.com>
2018-12-12 12:59:46 +0100

    Added bootstrap navbar to navigate through pages

--- src/Pages/Pages.js
+++ src/Pages/Pages.js
@@ -14,7 +14,7 @@ import App from "../App";
 
 import Paths from "../Paths/Paths";
 
-import LinkTest from "../components/LinkTest";
+//import LinkTest from "../components/LinkTest";
 
 /**
  * All routes go here.
@@ -24,9 +24,22 @@ class Pages extends Component {
   render() {
     return (
       <div>
-        <header>
-          <LinkTest />
-        </header>
+      <nav class="navbar navbar-default">
+          <div class="container-fluid">
+            <div class="navbar-header">
+              <a class="navbar-brand">Hitos API</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li class="active nav_item"><a href="landing">Home</a></li>
+              <li class="nav_item"><a href="login">Login</a></li>
+              <li class="nav_item"><a href="signup">Sign Up</a></li>
+              <li class="nav_item"><a href="milestones">Milestones</a></li>
+              <li class="nav_item"><a href="profile">Profile</a></li>
+              <li class="nav_item"><a href="search">Search</a></li>
+            </ul>
+          </div>
+        </nav>
+      
         <main>
           <Route path={Paths.Links.Landing} component={Landing} />
           <Route path={Paths.Links.Login} component={Login} />
