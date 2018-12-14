237dab7e3869555e4d5e77922d2fd4293505b279
Luis de la Cal <luiscal4a@gmail.com>
2018-12-14 15:54:54 +0100

    Added search by week functionality to search page

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -2,33 +2,61 @@ import React, { Component } from "react";
 import MilestoneList from "../../Components/MilestoneList";
 import axios from "axios";
 import Paths from "../../Paths/Paths";
-import SearchBar from "../../Components/searchBar"
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+//import SearchBar from "../../Components/searchBar"
 
 class Search extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      data: []
-	};
+      data: [],
+      searchMe: ""
+   };
+ }
 	
-	axios.get(`${Paths.Api.getByWeek}/10`).then(res => {
-		this.setState({
-		  data: res.data.milestones
-		});
-	});
+	
+
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
   }
 
-  handleSubmit =  event => {
+  handleSubmit = async event => {
     event.preventDefault();
 
-    console.log("Hey boyyy");
+    await axios.get(`${Paths.Api.getByWeek}/${this.state.searchMe}`).then(res => {
+      this.setState({
+        data: res.data.milestones
+      });
+    });
   }
+  
+
+  
 
 
   render() {
     return (
       <div className="Search">
-        <SearchBar/>
+        <form class="navbar-form" role="search" onSubmit={this.handleSubmit}>
+          <FormGroup controlId="searchMe" bsSize="large">
+            <FormControl
+              value={this.state.searchMe}
+              placeholder="search"
+              onChange={this.handleChange}
+              type="text"
+            />
+            <Button
+            block
+            bsSize="large"
+            type="submit"
+          >
+            Login
+          </Button>
+          </FormGroup>
+          
+        </form>
         <div>
         <MilestoneList data={this.state.data} />
         </div>
