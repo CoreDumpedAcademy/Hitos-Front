253bfb4bd9217cac9d6e0abcdcb69921751f81e6
LordMascachapas <32301199+LordMascachapas@users.noreply.github.com>
2018-12-20 17:13:24 +0100

    Profile page shows user's milestones

--- src/Pages/Profile/Profile.js
+++ src/Pages/Profile/Profile.js
@@ -1,28 +1,60 @@
 import React, { Component } from "react";
 import axios from 'axios';
 import UserData from '../../Components/userData';
+import MilestoneList from '../../Components/MilestoneList';
 
 class Profile extends Component {
 	constructor(props){
 		super(props);
+		var tempMilestoneList = [];
+		var tempUserData = {};
 		this.state = {
-			userData: {},
+			user: tempUserData,
+			milestones: tempMilestoneList,
 		};
 		axios.get('http://localhost:3000/user/5c015bd66aa4e2407c5cb650')
 		.then(res => {
-			this.setState({
-				userData: res.data.user
+			var i = 0;
+			res.data.user.milestonesCollection.map(data => {
+				axios.get(`http://localhost:3000/milestone/${res.data.user.milestonesCollection[i]._id}`)
+				.then(res2 => {
+					tempMilestoneList[i] = res2.data.milestone;
+					tempMilestoneList[i].status = res.data.user.milestonesCollection[i].status;
+					i++;
+					this.setState({
+						user: res.data.user,
+						milestones: tempMilestoneList,
+					});
+				});
 			});
 		});
 	}
-render() {
+
+	renderML(data) {
+		return(
+			<div>
+				<MilestoneList data={data} />
+			</div>
+		);
+	}
+
+	renderUD(data) {
+		return(
+			<div>
+				<UserData
+					username={data.userName}
+					role={data.role}
+					team={data.team}
+				/>
+			</div>
+		);
+	}
+
+	render() {
 		return (
 			<div className="Profile">
-				<UserData
-					username={this.state.userData.userName}
-					role={this.state.userData.role}
-					team={this.state.userData.team}
-				/>
+				{this.renderUD(this.state.user)}
+				{this.renderML(this.state.milestones)}
 			</div>
 		);
 	}
