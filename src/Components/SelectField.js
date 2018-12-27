f62d509cd4d4bdd1b01f64c8f6ac04e8484f6e22
LordMascachapas <alfredm3007@gmail.com>
2018-12-27 20:56:16 +0100

    SelectField component
    specify these propeties:
    title="someTitle"
    id="someId"
    placeholder="somePlaceHolder"
    options={ [ ... ] }
    onChange={ <<some event>> }
    value={ "SomeValue" }

---
+++ src/Components/SelectField.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "react-dropdown/style.css";
+
+const SelectField = props => {
+  return (
+    <FormGroup controlId={props.id}>
+      <ControlLabel>{props.title}</ControlLabel>
+      <FormControl componentClass="select" onChange={props.onChange}>
+        <option value={props.value} disabled selected>-- {props.placeholder} --</option>
+        {props.options.map(el =>
+          <option value={el}>{el}</option>
+        )}
+      </FormControl>
+    </FormGroup>
+  );
+};
+
+export default SelectField;
