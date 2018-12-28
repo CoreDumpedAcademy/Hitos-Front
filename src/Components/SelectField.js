68ffbf8c1ce0f20103af0bd46eeb520c314818ab
LordMascachapas <alfredm3007@gmail.com>
2018-12-28 18:37:34 +0100

    Fixes on SelectFields.js
    Identation and key property on map loop

--- src/Components/SelectField.js
+++ src/Components/SelectField.js
@@ -3,17 +3,17 @@ import { FormGroup, FormControl, ControlLabel } from "react-bootstrap";
 import "react-dropdown/style.css";
 
 const SelectField = props => {
-  return (
-    <FormGroup controlId={props.id}>
-      <ControlLabel>{props.title}</ControlLabel>
-      <FormControl componentClass="select" onChange={props.onChange}>
-        <option value={props.value} disabled selected>-- {props.placeholder} --</option>
-        {props.options.map(el =>
-          <option value={el}>{el}</option>
-        )}
-      </FormControl>
-    </FormGroup>
-  );
+	return (
+		<FormGroup controlId={props.id}>
+			<ControlLabel>{props.title}</ControlLabel>
+			<FormControl componentClass="select" onChange={props.onChange}>
+				<option value={props.value} disabled selected>-- {props.placeholder} --</option>
+				{props.options.map(el =>
+					<option value={el} key={el}>{el}</option>
+				)}
+			</FormControl>
+		</FormGroup>
+	);
 };
 
 export default SelectField;
